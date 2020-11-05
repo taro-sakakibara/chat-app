a67fb8b518b355d7a9057f2f8f15f79f790b3938
taro-sakakibara <theband0914@gmail.com>
2020-11-05 16:58:29 +0900

    メッセージの表示

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.include(:user)
   end
   
   
@@ -13,6 +14,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to room_messages_path
     else
+      @messages = @room.messages.includes(:user)
       render :index
     end
   end
